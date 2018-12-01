a310aaf64811e3b126580259375f7531d65d3a72
Poragon Gelé <poragonplays@gmail.com>
2018-12-01 15:53:58 -0500

    Update preload.lua

--- PoraComp/preload.lua
+++ PoraComp/preload.lua
@@ -1,3 +1,8 @@
+package.path = package.path .. ";/storage/emulated/0/Android/data/com.cleverraven.cataclysmdda/files/?.lua" --Android
+package.path = package.path .. ";/storage/sdcard/Android/data/com.cleverraven.cataclysmdda/files/?.lua" --Android (SD Card)
+package.path = package.path .. ";/storage/sdcard0/Android/data/com.cleverraven.cataclysmdda/files/?.lua" --Android (SD Card 0)
+package.path = package.path .. ";/storage/sdcard1/Android/data/com.cleverraven.cataclysmdda/files/?.lua" --Android (SD Card 1)
+
 require("data/mods/PoraComp/LUA/monster_attacks")
 require("data/mods/PoraComp/LUA/functions")
 require("data/mods/PoraComp/LUA/iuse_actions")
