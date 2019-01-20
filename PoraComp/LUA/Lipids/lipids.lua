d1dc190011fd16dacb096651db124c95a174a11d
Poragon <poragonplays@gmail.com>
2019-01-20 11:41:15 -0500

    v1.5.10
    Changed how fat works, no longer prevent hunger from going below a certain point. Now has a chance to reduce hunger based on current hunger and metabolic rate.
    
    Changed chance to and amount of fat stored per turn to be effected by metabolic rate

--- PoraComp/LUA/Lipids/lipids.lua
+++ PoraComp/LUA/Lipids/lipids.lua
@@ -1,5 +1,6 @@
 -- Quick notes 
 -- Fat amount is stored in the effect fat_reserves' intensity.
+-- 1 intensity of fat is 1 hunger worth of nutrition
 -- Ranges for weight effects are based upon an ideal fat percentage of 10 - 16.5% body weight and a weight of 62kg
 -- Ranges thusly are as follows (format is fat intensity(weight in kg), with 1 intensity of fat being 1g of fat);
 -- 1 (1g minimum_fat) - extremely underweight - 2170(2.17kg) - underweight - 6200(6.2kg) - ideal range - 10230(10.23kg) - overweight - 14260(14.26kg) - extremely overweight - 18290(18.29kg) - obesity - 20000(20kg maximum_fat)
@@ -12,6 +13,8 @@ local ideal_weight = efftype_id("ideal_weight")
 local overweight = efftype_id("overweight")
 local extremely_overweight = efftype_id("extremely_overweight")
 local obesity = efftype_id("obesity")
+local minimum_fat = 1
+local maximum_fat = 20000
 
 -- Coroutines
 
@@ -27,38 +30,38 @@ function fat_reserve_process()
 	-- Set up some variables each time called
 	local player_hunger = player:get_hunger()
 	local current_fat = player:get_effect_int(fat_reserves, "bp_torso") 
-	local minimum_fat = 1
-	local maximum_fat = 20000
-	local chance_1of5 = math.random(5)
-	new_fat_value = current_fat
+	local metabolic_rate = player:metabolic_rate()
+	local new_fat_value = current_fat
 	
-	-- Compares hunger a minute ago to current hunger. For every hunger value below 15 one point of hunger is converted to one level of fat intensity 33% of the time.
-	if player_hunger_old == nil then
-		player_hunger_old = player_hunger
-		return
-	elseif player_hunger < 15 and
-	current_fat <= maximum_fat and
-	chance_1of5 == 5 then
-		new_fat_value = current_fat + 1
-		player:set_hunger(player_hunger + 1)
-	end
+	-- Compares hunger a minute ago to current hunger. For every hunger value below 15 one point of hunger is converted to one level of fat intensity 10% of the time. Amount converted and chance are modified by metabolic rate.
 	
-	-- Draws from fat reserves when hunger gets above 75 to set it back to 75. 1:1 fat to hunger reduction, so one fat intensity is 9.7 kcal or just about 1.1g
-	if player_hunger > 75 and
-	current_fat > minimum_fat then
-		local hunger_mod = ( player_hunger - 75 )
-		if current_fat > hunger_mod then
-			new_fat_value = ( current_fat - hunger_mod )
-			player:set_hunger( player_hunger - hunger_mod )
-		elseif current_fat <= hunger_mod then
-			new_fat_value = ( 1 )
-			player:set_hunger( player_hunger + ( 1 - current_fat ) )
+	if player_hunger < 15 and
+	current_fat <= maximum_fat then
+		local fat_add_chance = ( 10 * metabolic_rate )
+		if math.random(100) < fat_add_chance then
+			local fat_add_amount = math.floor( ( math.random(2) * metabolic_rate ) )
+			new_fat_value = current_fat + fat_add_amount
+			player:set_hunger( player_hunger + fat_add_amount )
 		end
 	end
 	
-	-- Updates fat value, sets player_hunger_old again to prevent issues with hunger going negitive
+	-- Draws from fat reserves when hunger gets above 75. Chance for fat to reduce hunger linearly increases as hunger goes up, capping at a 75% chance to reduce hunger when the player is at 1399 hunger.
+	-- Amount drawn and chance to draw from fat reserves modified by metabolic rate
+	if player_hunger > 75 and
+	current_fat > minimum_fat then
+		local fat_draw_chance = ( ( (75/1399) * player_hunger ) * metabolic_rate )
+		local fat_draw_amount = math.floor( ( math.random(3) * metabolic_rate ) )
+		if math.random(100) < fat_draw_chance then
+			if fat_draw_amount >= current_fat then
+				fat_draw_amount = ( current_fat - 1 )
+			end
+			player:set_hunger( player_hunger - fat_draw_amount )
+			new_fat_value = ( current_fat - fat_draw_amount )
+		end	
+	end
+	
+	-- Updates fat value
 	player:add_effect(fat_reserves, TURNS(1), "bp_torso", true, new_fat_value)
-	player_hunger_old = player_hunger
 	
 	-- Updates current_fat, removes existing fat effects, applies effects based upon fat count 
 	if player:has_effect(extremely_underweight) == true then
